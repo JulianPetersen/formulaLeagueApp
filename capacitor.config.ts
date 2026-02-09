e5bfbc7210cffd67b96306954795ad9c0432e46d
Petersen <julian.petersen@cencosud.com.ar>
2026-02-09 15:25:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'formulaLeagueApp',
+  webDir: 'www'
+};
+
+export default config;
